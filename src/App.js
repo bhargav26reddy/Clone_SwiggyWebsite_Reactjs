924cc83db1bf1aceda91256786964e09599e0cb9
Sai Bhargav Reddy Maddireddy <maddireddysaireddy@gmail.com>
2024-07-11 09:43:19 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Appmain from './Components/Appmain';
 import { Provider } from 'react-redux';
 import Store from './Utilis/Store';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import Body from './Components/Body';
-import Contact from './Components/Cart';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
+
 
 
 function App() {
   return (
-    <Provider store={Store}>
+    <div>
+      <Provider store={Store}>
     <div className="App">
-      <Appmain />
+    <Appmain />
     </div>
-    </Provider>
+    </Provider> 
+
+    </div>
   );
 }
 
